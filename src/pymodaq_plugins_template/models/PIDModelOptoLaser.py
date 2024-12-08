70a912bd53c1caf96121634fedc1cfba628be3f1
nano713 <50872819+nano713@users.noreply.github.com>
2024-12-08 21:34:46 +0900

    Add suggestion. Needs to update based on the inherited class # DK - modules in the first line are obsolete. Import classes used in PIDModelGeneric -> https://github.com/PyMoDAQ/PyMoDAQ/blob/6daca234d2ba46a09f1ccebc9e982cb1b029d9ee/src/pymodaq/extensions/pid/utils.py#L32

--- src/pymodaq_plugins_template/models/PIDModelOptoLaser.py
+++ src/pymodaq_plugins_template/models/PIDModelOptoLaser.py
@@ -1,32 +1,38 @@
+# DK - modules in the first line are obsolete. Import classes used in PIDModelGeneric -> https://github.com/PyMoDAQ/PyMoDAQ/blob/6daca234d2ba46a09f1ccebc9e982cb1b029d9ee/src/pymodaq/extensions/pid/utils.py#L32
 from pymodaq.extensions.pid.utils import PIDModelGeneric, OutputToActuator, InputFromDetector, main
 from pymodaq.utils.data import DataToExport
 from typing import List
 
+# DK - comment out
 def power_covert_to_position(outputs: List[float], dt: float, stab=True):
     """ Should be replaced here or in the model class to process the outputs """
     #TODO: No simple way to do this
     return outputs
 
-
+# Dk - Comment out
 def some_function_to_convert_the_data(measurements: DataToExport):
     """ Should be replaced here or in the model class to process the measurement """
     a = 0
     b = 1
     return [a, b]
 
-
+# Rename PIDModelTemplate
 class PIDModelTemplate(PIDModelGeneric):
     limits = dict(max=dict(state=False, value=100),
                   min=dict(state=False, value=-100),)
     konstants = dict(kp=0.1, ki=0.000, kd=0.0000)
 
+    # DK - Nsetpoints = 1 because we move only one axis
     Nsetpoints = 2  # number of setpoints
+    # DK - [xxx] one element
     setpoint_ini = [128, 128]  # number and values of initial setpoints
+    # DK - ["xxx"] one element
     setpoints_names = ['Xaxis', 'Yaxis']  # number and names of setpoints
 
     actuators_name = ["X-KDC"]  # names of actuator's control modules involved in the PID
     detectors_name = ['PowerMeter']  # names of detector's control modules involved in the PID
 
+    # Target Power will be set in setpoint. Delete this dictionary. We only need wavelength at this moment.
     params = [{'title': 'Target Power', 'name': 'power', 'type': 'float', 'value': 0.01}, 
               {'title': 'Wavelength', 'name': 'wavelength', 'type': 'float', 'value': 0.001}] 
 
@@ -43,7 +49,7 @@ class PIDModelTemplate(PIDModelGeneric):
         """
         if param.name() == 'power':
             self.settings['power'] = param.value()
-        if param.name() == 'wavelegth':
+        if param.name() == 'wavelegth': # DK - correct typo
             self.settings['wavelength'] = param.value()
 
     def ini_model(self):
@@ -68,7 +74,7 @@ class PIDModelTemplate(PIDModelGeneric):
         power = power - self.settings['power']
         self.power = power
         #x, y = some_function_to_convert_the_data(measurements)
-        return InputFromDetector(values=[power])
+        return InputFromDetector(values=[power]) # DK - return in DataToExport object
 
     def convert_output(self, outputs: List[float], dt: float, stab=True):
         """
@@ -88,8 +94,10 @@ class PIDModelTemplate(PIDModelGeneric):
         """
         # outputs = power_covert_to_position(outputs, dt, stab)
         # return OutputToActuator(mode='rel', values=outputs)
+        #  DK restore return. Return in DataToActuators object.
         pass 
 
 
 if __name__ == '__main__':
+    # DK - rename .xml file although we have not created this xml yet.
     main("BeamSteeringMockNoModel.xml")  # some preset configured with the right actuators and detectors
