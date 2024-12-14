6b0620ae9fcdfb9ef417a2fcd0c0381911e0a885
nano713 <50872819+nano713@users.noreply.github.com>
2024-12-14 15:55:17 +0900

    Update convert_input and convert_output.

--- src/pymodaq_plugins_optocontroller/models/PIDModelOptoLaser.py
+++ src/pymodaq_plugins_optocontroller/models/PIDModelOptoLaser.py
@@ -3,8 +3,10 @@
 from typing import List
 import numpy as np
 from pymodaq.extensions.pid.utils import PIDModelGeneric, DataToActuatorPID, main
-from pymodaq.utils.data import DataActuator, DataToExport, DataCalculated
+from pymodaq.utils.data import DataActuator, DataToExport, DataCalculated, DataToActuators
 
+import logging
+logger = logging.getLogger(__name__)
 
 # def power_covert_to_position(outputs: List[float], dt: float, stab=True):
 #     """ Should be replaced here or in the model class to process the outputs """
@@ -24,18 +26,18 @@ class PIDModelOptoLaser(PIDModelGeneric):
     konstants = dict(kp=0.1, ki=0.000, kd=0.0000)
 
     Nsetpoints = 1  # number of setpoints
-    setpoint_ini = [128]  # number and values of initial setpoints
+    setpoint_ini = [1e-9]  # number and values of initial setpoints
     setpoints_names = ['power']  # number and names of setpoints
 
-    actuators_name = ['X-KDC']  # names of actuator's control modules involved in the PID
-    detectors_name = ['PowerMeter']  # names of detector's control modules involved in the PID
+    actuators_name = ['Move 00']  # names of actuator's control modules involved in the PID
+    detectors_name = ['Det 00']  # names of detector's control modules involved in the PID
 
     # Target Power will be set in setpoint. Delete this dictionary. We only need wavelength at this moment.
-    params = [ {'title': 'Wavelength', 'name': 'wavelength', 'type': 'float', 'value': 0.001}] 
+    params = [ {'title': 'Wavelength', 'name': 'wavelength', 'type': 'float', 'value': 532}]
 
     def __init__(self, pid_controller):
         super().__init__(pid_controller)
-        self.power = 0
+        # self.power = 0
 
     def update_settings(self, param):
         """
@@ -63,11 +65,16 @@ class PIDModelOptoLaser(PIDModelGeneric):
         DataToExport: the converted input in the setpoints units
 
         """
-        power = measurements.get_data_from_dim('PowerMeter')[0][0]
+        # power = measurements.data[0] # Edit
+        #get_data_from_dim(dim='Data0D')#[0][0]
+        power = measurements.get_data_from_dim('Data0D')[0][0]
+        self.curr_input = [power]
+        # logger.info("Current input: {}".format(self.curr_input))
         return DataToExport('inputs',
                     data=[DataCalculated(self.setpoints_names[ind],
-                                         data=[np.array([power])])
+                                         data=[self.curr_input[ind]]) # np.array([power])
                           for ind in range(len(self.setpoints_names))])
+
 
     def convert_output(self, outputs: List[float], dt: float, stab=True):
         """
@@ -85,10 +92,11 @@ class PIDModelOptoLaser(PIDModelGeneric):
         DataToActuatorPID: the converted output
 
         """
-        self.curr_output = outputs
+        self.curr_output = np.array(outputs)
         return DataToActuatorPID('pid', mode='rel',
-                         data=[DataActuator(self.actuators_name[ind], data=[self.curr_output[ind]])
+                         data=[DataActuator(self.actuators_name[ind], data=[self.curr_output])
                                for ind in range(len(self.curr_output))])
 
 if __name__ == '__main__':
-    main("OptoControllerModel.xml") 
+    # main("OptoControllerModel.xml")
+    main()
